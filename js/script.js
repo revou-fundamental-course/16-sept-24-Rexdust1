2034e1348d11c90fdb399c9f43e99a9b8f61b358
Rexdust <andy0412lie@gmail.com>
2024-09-25 01:09:27 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
 // ini javascript 
 
+function replaceName () {
+    let user = prompt("Silahkan diisi nama Anda!", "");
+    document.getElementById("home-user").innerHTML = user
+}
+
+document.getElementById("CYN").addEventListener("click", function () {
+    replaceName ();
+})
+
+document.getElementById
+
+function validateForm () {
+    const nama = document.forms[]
+    const birthDate = document.forms
+    const gender = document.forms
+    const messages = document.forms
+}
