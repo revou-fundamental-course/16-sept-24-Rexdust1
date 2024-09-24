da80762c958995040709c26a77010d1b762f0206
Rexdust <andy0412lie@gmail.com>
2024-09-25 02:22:53 +0700

    update

--- js/script.js
+++ js/script.js
@@ -12,8 +12,24 @@ document.getElementById("CYN").addEventListener("click", function () {
 document.getElementById
 
 function validateForm () {
-    const nama = document.forms[]
-    const birthDate = document.forms
-    const gender = document.forms
-    const messages = document.forms
+    const name = document.forms["message-form"]["form-nama"].value;
+    const birthDate = document.forms["message-form"]["form-tgllahir"].value;
+    const gender = document.forms["message-form"]["jk"].value;
+    const messages = document.forms["message-form"]["pesan"].value;
+
+    if (name == ""|| birthDate == "" || gender == "" || messages == "") {
+        alert("Required to fill in");
+        return false;
+    }
+
+    setSenderUI(name, birthDate, gender, messages);
+
+    return false;
+}
+
+function setSenderUI (name, birthDate, gender, messages) {
+    document.getElementById("div-nama").innerHTML = name;
+    document.getElementById("div-tgllahir").innerHTML = birthDate;
+    document.getElementById("div-jk").innerHTML = gender;
+    document.getElementById("div-pesan").innerHTML = messages;
 }
